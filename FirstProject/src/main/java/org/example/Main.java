8aa00cef8225f2084af64d6de7ddf8ab67068b86
pisgupta <p.is.gupta@gmail.com>
2026-08-07 14:44:11 +0530

    added fail test case with disable in gradle

--- FirstProject/src/main/java/org/example/Main.java
+++ FirstProject/src/main/java/org/example/Main.java
@@ -45,5 +45,7 @@ public class Main {
         System.out.println(m.mul(10,23));
         System.out.println(m.div(10,10));
         System.out.println(m.root(55));
+
+        System.out.println("Testing auto build generate");
     }
 }
