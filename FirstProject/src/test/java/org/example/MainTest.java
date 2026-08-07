ba12e56369ead7191eac63d151ba9a28252827f3
pisgupta <p.is.gupta@gmail.com>
2026-08-08 04:55:01 +0530

    added fail test case with disable in gradle

--- FirstProject/src/test/java/org/example/MainTest.java
+++ FirstProject/src/test/java/org/example/MainTest.java
@@ -84,7 +84,7 @@ class MainTest {
             int result = (int) invokePrivateMethod("div", new Class<?>[]{int.class, int.class}, 10, 23);
             assertEquals(0, result); // 10 / 23 = 0 in integer arithmetic
             int divResult = (int) invokePrivateMethod("div", new Class<?>[]{int.class, int.class}, 10, 2);
-            assertEquals(0,divResult);
+            assertEquals(5,divResult);
         }
 
         @Test
