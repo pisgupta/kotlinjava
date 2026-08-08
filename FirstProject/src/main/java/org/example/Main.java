3afac2d08aeef71188cfac7b89db9f7a94b1d722
pisgupta <p.is.gupta@gmail.com>
2026-08-08 17:27:00 +0530

    added fail test case with disable in gradle

--- FirstProject/src/main/java/org/example/Main.java
+++ FirstProject/src/main/java/org/example/Main.java
@@ -54,6 +54,7 @@ public class Main {
         System.out.println(m.div(10,10));
         System.out.println(m.root(55));
         System.out.println(m.isNumberIsPrime(11));
+        System.out.println("Hello");
 
     }
 }
